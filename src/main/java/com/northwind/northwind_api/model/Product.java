a06bbcc259d85e4c034ac9bfe5c6d33a72e72035
Yumozh <juliiamozhchil@gmail.com>
2026-06-15 15:38:35 -0500

    create a model

---
+++ src/main/java/com/northwind/northwind_api/model/Product.java
@@ -0,0 +1,111 @@
+package com.northwind.northwind_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "ProductName")
+    private String productName;
+    @Column(name = "SupplierID")
+    private int supplierId;
+    @Column(name = "CategoryID")
+    private int categoryId;
+    @Column(name = "QuantityPerUnit")
+    private String quantityPerUnit;
+    @Column(name = "UnitPrice")
+    private float unitPrice;
+    @Column(name = "UnitsInStock")
+    private int unitsInStock;
+    @Column(name = "UnitsOnOrder")
+    private int unitsOnOrder;
+    @Column(name = "ReorderLevel")
+    private int reorderLevel;
+    @Column(name = "Discontinued")
+    private byte discontinued;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getQuantityPerUnit() {
+        return quantityPerUnit;
+    }
+
+    public void setQuantityPerUnit(String quantityPerUnit) {
+        this.quantityPerUnit = quantityPerUnit;
+    }
+
+    public float getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public int getUnitsInStock() {
+        return unitsInStock;
+    }
+
+    public void setUnitsInStock(int unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public int getUnitsOnOrder() {
+        return unitsOnOrder;
+    }
+
+    public void setUnitsOnOrder(int unitsOnOrder) {
+        this.unitsOnOrder = unitsOnOrder;
+    }
+
+    public int getReorderLevel() {
+        return reorderLevel;
+    }
+
+    public void setReorderLevel(int reorderLevel) {
+        this.reorderLevel = reorderLevel;
+    }
+
+    public byte getDiscontinued() {
+        return discontinued;
+    }
+
+    public void setDiscontinued(byte discontinued) {
+        this.discontinued = discontinued;
+    }
+}
